50fca59bb979fdda541fb466dc94b5cd59827750
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-08-26 00:02:55 +0530

    firebase setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizeFonts: true,
+    optimizeImages: true,
+    scrollRestoration: true,
+    appDir: true,
+  },
+  images: {
+    domains: [],
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
